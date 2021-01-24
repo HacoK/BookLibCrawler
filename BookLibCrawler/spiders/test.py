961c7a7eec71b2488e33b6cfdd28242743530223
HacoK <haco9812@gmail.com>
2021-01-24 23:33:27 +0800

    finish job-position crawling in security area

--- BookLibCrawler/spiders/test.py
+++ BookLibCrawler/spiders/test.py
@@ -187,3 +187,55 @@
 #             pass
         
 #     file.close()
+
+#5.深信服
+
+#coding=utf-8
+import json
+import requests
+# 1.
+# with open('text.txt', 'w', encoding='UTF-8') as f:
+#         url = "https://hr.sangfor.com/api/api/Jobs"
+#         data = json.dumps({"channelId":110,"page":1,"pageSize":600,"departmentId":0,"functionId":0,"kw":"","locationId":0,"workPlaceId":0})
+#         headers = {
+#             'Content-Type':'application/json;charset=UTF-8',
+#             'Host':'hr.sangfor.com',
+#             'Origin':'https://hr.sangfor.com',
+#             'Referer':'https://hr.sangfor.com/Sociology',
+#             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+#             'Accept': 'application/json, text/plain, */*',
+#             'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImJmMU1KWlhvc0lhYjZ0OW1NV1VtMEEiLCJ0eXAiOiJhdCtqd3QifQ.eyJuYmYiOjE2MTE0OTU5ODYsImV4cCI6MTYxMTQ5OTU4NiwiaXNzIjoiaHR0cDovLzIwMC4yMDAuMS41Nzo4MDkwIiwiYXVkIjoiYXBpIiwiY2xpZW50X2lkIjoic2FuZ2Zvcl9jbGllbnQiLCJzY29wZSI6WyJhcGkiXX0.qaWvtwzlQxo1lWBKdhcG2E2AVibUCZbVOmS7ridIk59mrz_Ohchy970x9E6nSR8liPi0CcDqsYSP8hf2gGWHRSBo0rAn0oR6a7zF-Q6lRq8_D1BRsr5Tai4qm9e094fnOPnXFQefavJ30fElwUym_MB6ASDCBZjjmoirqq5rkBd2VgTGiesZRAxCk2MgGQzIBf-Q3pFtr1BzPmvdz1CLGTd0bRVmPLmhE6_lDIaexE0YfB8LElm6JjXZQsGA5QNkh5t1e-G-zNJ3YxK9O0LweMpFJESPEI8zIj1cnKMk1_5zfvnCh7Grpb_Qh1rvNlxyfJebjffBxNwYmPFsLCYQUw',
+#             'Cookie':'ticket=eyJhbGciOiJSUzI1NiIsImtpZCI6ImJmMU1KWlhvc0lhYjZ0OW1NV1VtMEEiLCJ0eXAiOiJhdCtqd3QifQ.eyJuYmYiOjE2MTE0OTU5ODYsImV4cCI6MTYxMTQ5OTU4NiwiaXNzIjoiaHR0cDovLzIwMC4yMDAuMS41Nzo4MDkwIiwiYXVkIjoiYXBpIiwiY2xpZW50X2lkIjoic2FuZ2Zvcl9jbGllbnQiLCJzY29wZSI6WyJhcGkiXX0.qaWvtwzlQxo1lWBKdhcG2E2AVibUCZbVOmS7ridIk59mrz_Ohchy970x9E6nSR8liPi0CcDqsYSP8hf2gGWHRSBo0rAn0oR6a7zF-Q6lRq8_D1BRsr5Tai4qm9e094fnOPnXFQefavJ30fElwUym_MB6ASDCBZjjmoirqq5rkBd2VgTGiesZRAxCk2MgGQzIBf-Q3pFtr1BzPmvdz1CLGTd0bRVmPLmhE6_lDIaexE0YfB8LElm6JjXZQsGA5QNkh5t1e-G-zNJ3YxK9O0LweMpFJESPEI8zIj1cnKMk1_5zfvnCh7Grpb_Qh1rvNlxyfJebjffBxNwYmPFsLCYQUw; websiiteconfig={%22siteId%22:1%2C%22webSiteName%22:%22%E6%B7%B1%E4%BF%A1%E6%9C%8D%E6%8B%9B%E8%81%98%E5%AE%98%E7%BD%91%22%2C%22title%22:%22%E6%B7%B1%E4%BF%A1%E6%9C%8D%E6%8B%9B%E8%81%98%E5%AE%98%E7%BD%91%22%2C%22logoUrl%22:%22http://200.200.4.54:8100/hr-system/zhaopin/2020/08/10/98b48888e1544512a663e04eb5c843e8.jpg%22%2C%22domainName%22:%22https://zhaopin.sangfor.com%22%2C%22keywords%22:%22%E6%B7%B1%E4%BF%A1%E6%9C%8D%E6%8B%9B%E8%81%98%E5%AE%98%E7%BD%91%22%2C%22theme%22:%22#%22%2C%22copyright%22:%22<p>&copy%3B2000-2020%20%E6%B7%B1%E4%BF%A1%E6%9C%8D%E7%A7%91%E6%8A%80%E8%82%A1%E4%BB%BD%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8%20%E7%89%88%E6%9D%83%E6%89%80%E6%9C%89%20%E7%B2%A4ICP%E5%A4%8708126214%E5%8F%B7</p>%22%2C%22description%22:%22%E6%B7%B1%E4%BF%A1%E6%9C%8D%E6%8B%9B%E8%81%98%E5%AE%98%E7%BD%91%22}',
+#             'X-Requested-With': 'XMLHttpRequest'
+#         }
+#         r = requests.post(url=url,data =data,headers=headers)
+#         json.dump(r.json(),f,ensure_ascii=False)
+
+# 2.
+# with open('text.txt', 'r', encoding='UTF-8') as f:
+#     data = json.load(f)['data']['listData']
+#     file = open('data.txt', 'w', encoding='UTF-8')
+#     for job in data:
+#         if job['functionName']!='中高端岗位':
+#             continue
+#         title = job['title']
+#         departmentName = job['departmentName']
+#         commitment = job['commitment']
+#         workPlaceText = job['workPlaceText']
+#         description = job['description']
+#         if description:
+#             description = description.replace('<p>','').replace('</p>','\n').replace('<br>','').replace('&nbsp;','').replace('<strong>','')
+
+#         result = title+'\n'
+#         if departmentName:
+#             result += departmentName+'/'
+#         result += commitment+'\n'
+#         result += '工作城市：'+workPlaceText+'\n'
+#         if description:
+#             result += description
+#         result+='\n'
+
+#         file.write(result)
+
+#     file.close()
+
