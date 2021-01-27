e0898de837342e319f2b7d6715de88a8f172bb7a
HacoK <haco9812@gmail.com>
2021-01-27 17:35:06 +0800

    add liepin crawler && updata book records

--- BookLibCrawler/spiders/all_spider.py
+++ BookLibCrawler/spiders/all_spider.py
@@ -18,8 +18,7 @@ class BookLibSpider(scrapy.Spider):
 
     data = pd.read_csv('C:/Users/30438/Desktop/origin.csv')
     isbns = data['isbn'].tolist()
-    count = 50579
-    
+    count = 62475
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
     }
--- BookLibCrawler/spiders/test.py
+++ BookLibCrawler/spiders/test.py
@@ -190,9 +190,9 @@
 
 #5.深信服
 
-#coding=utf-8
-import json
-import requests
+# # coding=utf-8
+# import json
+# import requests
 # 1.
 # with open('text.txt', 'w', encoding='UTF-8') as f:
 #         url = "https://hr.sangfor.com/api/api/Jobs"
@@ -239,3 +239,104 @@ import requests
 
 #     file.close()
 
+# # 猎聘
+# # https://www.liepin.com/company-jobs/8905176/
+# import scrapy
+# import time
+# from scrapy_selenium import SeleniumRequest
+
+# class LiepinSpider(scrapy.Spider):
+#     name = 'liepin'
+#     headers = {
+#         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+#         'Host': 'www.liepin.com',
+#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#         'Accept-Encoding': 'gzip, deflate, br',
+#         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+#         'Cookie': '__uuid=1611425987928.23; __s_bid=3315952b39c79092ac6db64f7e543dc049d0; _fecdn_=1; acw_tc=3ccdc15116117109461821646e3f0f62c80143f92040f12e40f79e56f435a6; JSESSIONID=506C910A636E91FA93BD54D7157C592F; fe_se=-1611711706304; __tlog=1611710948063.61%7C00000000%7CR000000075%7C00000000%7C00000000; __session_seq=11; __uv_seq=46',
+#     }
+#     index='https://www.liepin.com/company-jobs/7923384/'
+
+#     def start_requests(self):
+#         yield SeleniumRequest(url=self.index)
+
+#     def parse(self, response):
+#         time.sleep(1)
+#         total_page = int(response.css('#company div.main section div div div:nth-child(2) div div a.last::attr(number)').get())
+#         total_page += 2
+#         for cur_page in range(1,total_page):
+#             yield scrapy.Request(url=self.index+'pn'+str(cur_page)+'/',callback=self.extra_list,headers=self.headers)
+
+#     def extra_list(self, response):
+#         anchors = response.css('#company div.main section div div div:nth-child(2) ul li div.job-info a.title::attr(href)') .getall()
+#         for anchor in anchors:
+#             yield scrapy.Request(url=anchor,callback=self.parse_detail,headers=self.headers)
+
+#     def parse_detail(self, response):
+#         title = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div.title-info > h1::text').get()
+#         salary = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > p.job-item-title::text').get().strip()[:-1]
+#         location = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > p.basic-infor > span > a::text').get()
+        
+#         education = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > div > span:nth-child(1)::text').get()
+#         experience = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > div > span:nth-child(2)::text').get()
+        
+#         description = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div.job-item.main-message.job-description > div.content').re(r'>(.*)<')
+#         if len(description)==0:
+#             description = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div.job-item.main-message.job-description > div.content::text').get()
+#         else:
+#             description = description[0]
+#         description = description.replace('\xa0','').replace('<br>','\n')
+
+#         yield {'岗位名称':title,'薪资水平':salary,'工作地点':location,'学历要求':education,'工作经验':experience,'工作描述':description}
+        
+# 猎聘 vip
+# https://vip.liepin.com/7893220/joblist.shtml
+# import scrapy
+# import time
+# from scrapy_selenium import SeleniumRequest
+
+# class LiepinSpider(scrapy.Spider):
+#     name = 'liepin'
+#     headers = {
+#         'Host': 'www.liepin.com',
+#     'Accept': 'application/json, text/javascript, */*; q=0.01',
+#     'X-Requested-With': 'XMLHttpRequest',
+#     'X-Alt-Referer': 'https://vip.liepin.com/7893220/joblist.shtml',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+#     'Referer': 'https://www.liepin.com/ajaxproxy.html',
+#     'Accept-Encoding': 'gzip, deflate, br',
+#     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+#     'Cookie': '__uuid=1611425987928.23; __s_bid=3315952b39c79092ac6db64f7e543dc049d0; _fecdn_=1; fe_se=-1611711706304; acw_tc=2760827816117366543086341e7472cec8b10f8545b9ef2839d0fd446394d1; __tlog=1611710948063.61%7C%7C%7C00000000%7C00000000; _mscid=00000000; _uuid=43027849FFA844220848574C6311AD80; JSESSIONID=95E94E7BC2B57E524478FEC91AF5FD07; __session_seq=112; __uv_seq=147'
+#     }
+#     index='https://vip.liepin.com/7893220/joblist.shtml'
+
+#     def start_requests(self):
+#         yield SeleniumRequest(url=self.index)
+
+#     def parse(self, response):
+#         time.sleep(1)
+#         total_page = 50
+#         for cur_page in range(0,total_page):
+#             yield scrapy.Request(url='https://www.liepin.com/company/sojob.json?pageSize=15&curPage='+str(cur_page)+'&ecompIds=7893220&dq=&publishTime=&keywords=&_=1611737867208',callback=self.extra_list,headers=self.headers)
+
+#     def extra_list(self, response):
+#         anchors = map(lambda x:x['url'],response.json()['data']['list'])
+#         for anchor in anchors:
+#             yield scrapy.Request(url=anchor,callback=self.parse_detail,headers=self.headers)
+
+#     def parse_detail(self, response):
+#         title = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div.title-info > h1::text').get()
+#         salary = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > p.job-item-title::text').get().strip()[:-1]
+#         location = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > p.basic-infor > span > a::text').get()
+        
+#         education = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > div > span:nth-child(1)::text').get()
+#         experience = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div:nth-child(2) > div.clearfix > div.job-title-left > div > span:nth-child(2)::text').get()
+        
+#         description = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div.job-item.main-message.job-description > div.content').re(r'>(.*)<')
+#         if len(description)==0:
+#             description = response.css('#job-view-enterprise > div.wrap.clearfix > div.clearfix > div.main > div.about-position > div.job-item.main-message.job-description > div.content::text').get()
+#         else:
+#             description = description[0]
+#         description = description.replace('\xa0','').replace('<br>','\n')
+
+#         yield {'岗位名称':title,'薪资水平':salary,'工作地点':location,'学历要求':education,'工作经验':experience,'工作描述':description}
